47d3700f7d57f39d95cc2d7823ea03f82256928f
Anatoly Kuzmin <tolaykuzay@gmail.com>
2019-11-19 03:53:57 +0300

    leftbar upd(dinamic)

--- src/LeftBar.js
+++ src/LeftBar.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 class LeftBar extends Component {
+	state = {
+		menuItems:[
+			{
+				id:1,
+				name:'First'
+			},
+			{
+				id:2,
+				name:'Second'
+			},
+			{
+				id:3,
+				name:'Thierd'
+			}
+		]
+
+		
+	}
 	render(){
 		return (
 			<div id="leftbar">
 				<ul>
-					<li>item1</li>
-					<li>itrm2</li>
-					<li>item3</li>
-					<li>item4</li>
+				{this.state.menuItems.map(item => <li id={item.id}>{item.name}</li>)}
 				</ul>
+			
 			</div>
 		)
 	}
